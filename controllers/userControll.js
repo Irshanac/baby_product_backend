ad68844ed9d3bbe32b60f13548cf964515fae5b5
Irshanac <irshana951@gmail.com>
2024-11-25 16:38:18 +0430

    login change by token

--- controllers/userControll.js
+++ controllers/userControll.js
@@ -1,7 +1,8 @@
 import user from '../models/userModels.js'
 import bcrypt from 'bcrypt'
-import generateToken from '../utils/jwt.js';
+import {generateToken} from '../utils/jwt.js';
 import asyncErrorResolver from "../middlewares/asyncErrorResolver.js"
+import CustomError from '../utils/customError.js';
 export const registaerUser=asyncErrorResolver(async(req,res)=>{
     const {username,name,password,email}=req.body
     const existingUser=await user.findOne({email})
@@ -20,16 +21,24 @@ export const loginUser=asyncErrorResolver(async(req,res)=>{
     const userData=await user.findOne({email})
     if(!userData)
     {
-        return res.status(400).json({message:"please create a account"})
+        throw new CustomError("please create a account", 400);
     }
     else{
         const isMatch = await bcrypt.compare(password, userData.password);
         
-        if (isMatch) {
-            return res.send(`Please welcome, ${userData.name}`);
+        if (!isMatch) {
+            throw new CustomError("Invalid  Password, Try Again", 400);
         }
-        else{
-            return res.status(400).json({ message: "Please enter the correct password." });
-        }
+        const token = generateToken(userData._id);
+        res.status(200).json({
+            status: "success",
+            message: "Login successful",
+            token,
+            user: {
+              id: userData._id,
+              name: userData.name,
+              email: userData.email,
+            },
+        })
     }
 })
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,9 +1,16 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken = (userId) => {
+export const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
 };
+export const verifyTokenGenerated = (token) => {
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+        console.error("Error verifying token:", error.message);
+        throw new Error("Invalid or expired token");
+    }
+};
 
-export default generateToken;
